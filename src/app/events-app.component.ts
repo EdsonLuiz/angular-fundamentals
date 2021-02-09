a35d3985de82d49740d37a048013bdb602fb5af0
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-09 13:59:49 -0300

    feat: add navbar

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: `
+    <app-navbar></app-navbar>
     <app-events-list></app-events-list>
   `,
   styles: []
